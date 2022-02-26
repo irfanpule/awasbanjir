6028bb6956655bca391463b0eb91893788f11b1e
irfanpule <irfan.biankalang@gmail.com>
2022-02-26 14:04:36 +0700

    change login view use default django

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,5 @@ from website import views
 app_name = 'website'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
 ]
--- website/views.py
+++ website/views.py
@@ -8,13 +8,6 @@ def index(request):
     return render(request, 'website/index.html', context)
 
 
-def signin(request):
-    context = {
-        'title_page': "Masuk"
-    }
-    return render(request, 'website/signin.html', context)
-
-
 def signup(request):
     context = {
         'title_page': "Daftar"
