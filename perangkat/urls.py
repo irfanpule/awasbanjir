6c62b5561ead257652dba47de1fe47a32cee130d
irfanpule <irfan.biankalang@gmail.com>
2022-02-26 15:01:56 +0700

    style list perangkat

--- perangkat/urls.py
+++ perangkat/urls.py
@@ -4,6 +4,6 @@ from perangkat import views
 app_name = 'perangkat'
 urlpatterns = [
     path('list/', views.PerangkatListView.as_view(), name='list'),
-    path('create/', views.PerangkatCrateView.as_view(), name='create_device'),
-    path('update/<int:pk>', views.PerangkatUpdateView.as_view(), name='update_device'),
+    path('create/', views.PerangkatCrateView.as_view(), name='create'),
+    path('update/<int:pk>', views.PerangkatUpdateView.as_view(), name='update'),
 ]
