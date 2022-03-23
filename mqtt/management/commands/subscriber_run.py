a7e1d4571ae7c497708ab888073133a70d7f6091
irfanpule <irfan.biankalang@gmail.com>
2022-03-23 21:34:40 +0700

    modify listener use broker auth and handle except

--- mqtt/management/commands/subscriber_run.py
+++ mqtt/management/commands/subscriber_run.py
@@ -1,14 +1,23 @@
 import json
+import asyncio
 
 from paho.mqtt import client as mqtt_client
+from json.decoder import JSONDecodeError
+
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
+from django.core.exceptions import ValidationError
 from api.serializers import DataSeriesSerializer
 from perangkat.models import Perangkat
+from warga.models import Warga
+from awasbanjir import notifications
 
 
-broker = 'test.mosquitto.org'
+broker = 'broker.emqx.io'
 port = 1883
-topic = "python/mqtt"
+topic = "awas_banjir_bagelen"
+username = 'nanonoa'
+password = 'best'
 
 
 def connect_mqtt() -> mqtt_client:
@@ -19,26 +28,50 @@ def connect_mqtt() -> mqtt_client:
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client()
+    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
 
 def subscribe(client: mqtt_client):
+    def get_device_cache(device_id, set_status):
+        status = cache.get(device_id)
+        if not status:
+            cache.set(device_id, set_status)
+            status = set_status
+        return status
+
     def on_message(client, userdata, msg):
-        data_str = msg.payload.decode()
-        data_dict = json.loads(data_str)
-        print(f"Received `{data_str}` from `{msg.topic}` topic")
+        try:
+            data_str = msg.payload.decode()
+            data_dict = json.loads(data_str)
+            print(f"Received `{data_str}` from `{msg.topic}` topic")
+        except JSONDecodeError:
+            print(msg.payload)
+            return
+
         serializer = DataSeriesSerializer(data=data_dict)
         if serializer.is_valid():
-            print("valid")
-            perangkat = Perangkat.objects.filter(device_id=data_dict['device_id']).first()
-            if perangkat:
-                data_series = serializer.save(perangkat=perangkat)
-                data_series.set_status()
-                print("berhasil simpen data")
-            else:
-                print("gak ada perangkat")
+            try:
+                perangkat = Perangkat.objects.filter(device_id=data_dict['device_id']).first()
+                if perangkat:
+                    data_series = serializer.save(perangkat=perangkat)
+                    data_series.set_status()
+                    status = get_device_cache(data_dict['device_id'], data_series.status)
+                    print(status, data_series.status)
+                    if status != data_series.status:
+                        print("kirim notif ", data_series.get_status_display())
+                        cache.set(data_dict['device_id'], data_series.status)
+                        for warga in Warga.objects.all():
+                            n = notifications.send_telegram_personal(
+                                warga.no_hp, f"Awas Banjir!, status {data_series.get_status_display()}")
+                            asyncio.run(n)
+                else:
+                    print("gak ada perangkat")
+            except ValidationError:
+                print("UUID tidak ditemukan")
+
         else:
             print("data not valid")
 
