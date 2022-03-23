4d5b50ee2fe2625a8285a1ff071afb700a8cbdc4
irfanpule <irfan.biankalang@gmail.com>
2022-03-23 21:45:03 +0700

    add model mqtt broker

--- mqtt/models.py
+++ mqtt/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class MQTTBroker(models.Model):
+    broker = models.CharField(max_length=200)
+    port = models.IntegerField()
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.broker
